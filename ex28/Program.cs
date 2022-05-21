c87e7fdd41aca80c5dbb719681b484db7d9b5e56
OlegLegezo <oleglegezo@yandex.ru>
2022-05-21 21:27:42 +0300

    реш 28

---
+++ ex28/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+
+
+Console.WriteLine("Введите любое натуральное число");
+int number = int.Parse(Console.ReadLine());
+double mul = 1;
+for (int i = 1; i <= number; i++)
+{
+    mul = mul * i;
+}
+Console.WriteLine($"{number} -> {mul}");
