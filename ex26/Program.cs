1911a50323f41ac2f6a14a8ccb33e4f1f0dbfe0d
OlegLegezo <oleglegezo@yandex.ru>
2022-05-21 21:27:27 +0300

    реш 26

---
+++ ex26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+Console.WriteLine("введите натуральное число");
+int number = int.Parse (Console.ReadLine());
+int count = 0;
+Console.Write($"кол-во цифр в числе{number} = ");
+while (number >0)
+{
+    number=number/10;
+    count++;
+}
+Console.WriteLine(count);
