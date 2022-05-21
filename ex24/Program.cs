068e330dff1b20e16842e36504a5d481aff51656
OlegLegezo <oleglegezo@yandex.ru>
2022-05-21 21:27:06 +0300

    реш 24

---
+++ ex24/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// найдите сумму чисел введенного числа
+
+
+
+
+Console.Write($"введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + i; //- короткая запись sum+=i
+}
+Console.Writeline($"сумма чисел от 1 до {number} = {sum}");
